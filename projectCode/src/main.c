0632925f8fc851751447c8d2992d56e4c511834a
Adham <1201199@student.birzeit.edu>
2024-11-25 09:31:57 +0200

    adding semaphore to mover

--- projectCode/src/main.c
+++ projectCode/src/main.c
@@ -19,15 +19,16 @@ pid_t *inspectors2_pid;
 pid_t *inspectors3_pid;
 
 key_t shm_gen_calc_key, sem_gen_calc_key, msg_gen_calc_key;
-key_t sem_inspector1_key , sem_inspector2_key;
+key_t sem_inspector1_key , sem_inspector2_key , sem_mover_key;
 int shm_id = -1, sem_id = -1, msg_id = -1;
-int sem_inspector1_id = -1 , sem_inspector2_id = -1 ;
+int sem_inspector1_id = -1 , sem_inspector2_id = -1 , sem_mover_id = -1;
 void cleanup() {
     if (shm_id != -1) shmctl(shm_id, IPC_RMID, NULL);
     if (sem_id != -1) semctl(sem_id, 0, IPC_RMID);
     if (msg_id != -1) msgctl(msg_id, IPC_RMID, NULL);
     if (sem_inspector1_id != -1) semctl(sem_inspector1_id, 0, IPC_RMID);
     if (sem_inspector2_id != -1) semctl(sem_inspector2_id, 0, IPC_RMID);
+    if (sem_mover_id != -1) semctl(sem_mover_id, 0, IPC_RMID);
 
     free(generators_pid);
     free(calculators_pid);
@@ -117,6 +118,14 @@ int main(int argc, char *argv[]) {
         cleanup();
         exit(1);
     }
+    // Create semaphore key for mover
+    key_t sem_gen_mover_key = ftok(".",'K');
+    if (sem_mover_key == -1) {
+        perror("Semaphore key generation failed");
+        cleanup();
+        exit(1);
+    }
+
 
         struct sembuf {
         unsigned short sem_num;  // Semaphore number in the set
@@ -163,6 +172,18 @@ int main(int argc, char *argv[]) {
         cleanup();
         exit(1);
     }
+    // Create semaphore for mover
+    sem_mover_id = semget(sem_gen_mover_key, 1, IPC_CREAT | 0666);
+    if (sem_mover_id == -1) {
+        perror("Semaphore creation failed");
+        cleanup();
+        exit(1);
+    }
+    if (semctl(sem_mover_id, 0, SETVAL, 1) == -1) {
+        perror("Semaphore initialization failed");
+        cleanup();
+        exit(1);
+    }
 
 
     
@@ -187,14 +208,16 @@ int main(int argc, char *argv[]) {
     snprintf(sem_key_str, sizeof(sem_key_str), "%d", sem_gen_calc_key);
     snprintf(msg_key_str, sizeof(msg_key_str), "%d", msg_gen_calc_key);
     setenv("MSG_QUEUE_GC_KEY", msg_key_str, 1);
-    char sem_inspector1_key_str[20] , sem_inspector2_key_str[20];
+    char sem_inspector1_key_str[20] , sem_inspector2_key_str[20] , sem_mover_key_str[20];
     snprintf(sem_inspector1_key_str, sizeof(sem_inspector1_key_str), "%d", sem_gen_insp1_key);
     snprintf(sem_inspector2_key_str, sizeof(sem_inspector2_key_str), "%d", sem_gen_insp2_key);
+    snprintf(sem_mover_key_str, sizeof(sem_mover_key_str), "%d", sem_gen_mover_key);
 
     // Start child processes
     generators_pid = (pid_t *)malloc(config.NUM_GENERATORS * sizeof(pid_t));
     for (int i = 0; i < config.NUM_GENERATORS; i++) {
         if ((generators_pid[i] = fork()) == 0) {
+            // execl("./bin/generator", "generator", argv[1], shm_key_str, sem_key_str, NULL);
             execl("/home/adduser/ENCS4330/Projects/Project2/File-Management-Simulation/projectCode/bin/generator", "generator", argv[1], shm_key_str, sem_key_str, NULL);
             perror("Generator process failed");
             exit(1);
@@ -227,6 +250,7 @@ int main(int argc, char *argv[]) {
     calculators_pid = (pid_t *)malloc(config.NUM_CALCULATORS * sizeof(pid_t));
     for (int i = 0; i < config.NUM_CALCULATORS; i++) {
         if ((calculators_pid[i] = fork()) == 0) {
+            // execl("./bin/calculator", "calculator", argv[1], NULL);
             execl("/home/adduser/ENCS4330/Projects/Project2/File-Management-Simulation/projectCode/bin/calculator", "calculator", argv[1], NULL);
             perror("Calculator process failed");
             exit(1);
@@ -236,7 +260,8 @@ int main(int argc, char *argv[]) {
     movers_pid = (pid_t *)malloc(config.NUM_MOVERS * sizeof(pid_t));
     for (int i = 0; i < config.NUM_MOVERS; i++) {
         if ((movers_pid[i] = fork()) == 0) {
-            execl("/home/adduser/ENCS4330/Projects/Project2/File-Management-Simulation/projectCode/bin/mover", "mover", argv[1], NULL);
+            // execl("./bin/mover", "mover", argv[1], sem_mover_key_str, NULL);
+            execl("/home/adduser/ENCS4330/Projects/Project2/File-Management-Simulation/projectCode/bin/mover", "mover", argv[1],sem_mover_key_str, NULL);
             perror("Mover process failed");
             exit(1);
         }
@@ -245,6 +270,7 @@ int main(int argc, char *argv[]) {
     inspectors1_pid = (pid_t *)malloc(config.NUM_INSPECTOR1 * sizeof(pid_t));
     for (int i = 0; i < config.NUM_INSPECTOR1; i++) {
         if ((inspectors1_pid[i] = fork()) == 0) {
+            // execl("./bin/inspector1", "inspector1", argv[1],sem_inspector1_key_str, NULL);
             execl("/home/adduser/ENCS4330/Projects/Project2/File-Management-Simulation/projectCode/bin/inspector1", "inspector1", argv[1],sem_inspector1_key_str, NULL);
             perror("Inspector1 process failed");
             exit(1);
@@ -254,6 +280,7 @@ int main(int argc, char *argv[]) {
     inspectors2_pid = (pid_t *)malloc(config.NUM_INSPECTOR2 * sizeof(pid_t));
     for (int i = 0; i < config.NUM_INSPECTOR2; i++) {
         if ((inspectors2_pid[i] = fork()) == 0) {
+            // execl("./bin/inspector2", "inspector2", argv[1],sem_inspector2_key_str, NULL);
             execl("/home/adduser/ENCS4330/Projects/Project2/File-Management-Simulation/projectCode/bin/inspector2", "inspector2", argv[1],sem_inspector2_key_str, NULL);
             perror("Inspector2 process failed");
             exit(1);
@@ -263,6 +290,7 @@ int main(int argc, char *argv[]) {
     inspectors3_pid = (pid_t *)malloc(config.NUM_INSPECTOR3 * sizeof(pid_t));
     for (int i = 0; i < config.NUM_INSPECTOR3; i++) {
         if ((inspectors3_pid[i] = fork()) == 0) {
+            // execl("./bin/inspector3", "inspector3", argv[1], NULL);
             execl("/home/adduser/ENCS4330/Projects/Project2/File-Management-Simulation/projectCode/bin/inspector3", "inspector3", argv[1], NULL);
             perror("Inspector3 process failed");
             exit(1);
--- projectCode/src/mover.c
+++ projectCode/src/mover.c
@@ -5,7 +5,7 @@ Config config;
 
 
 int main (int argc , char * argv[]){
-    if (argc != 2) {
+    if (argc != 3) {
         fprintf(stderr, "Usage: %s <config file>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -14,7 +14,21 @@ int main (int argc , char * argv[]){
         fprintf(stderr, "Error loading config file\n");
         exit(EXIT_FAILURE);
     }
+    
+    key_t sem_key = atoi(argv[2]);
+    int sem_id = semget(sem_key, 1, 0666);
+    if (sem_id == -1) {
+        perror("Semaphore retrieval failed");
+        return 1;
+    }
 
+    int sem_value = semctl(sem_id, 0, GETVAL);
+    if (sem_value == -1) {
+        perror("Failed to get semaphore value");
+        exit(1);
+    }
+
+    sem_wait(sem_id);
 
     if(!dirExists(processesdDir)){
         
@@ -22,6 +36,7 @@ int main (int argc , char * argv[]){
         createDirectory(processesdDir);
         
     }
+    sem_signal(sem_id);
     
 
         ////////////////////////////
